62dc99e548e3494c05d5d7fead6cc79583cdd57e
Steve Albonico <salbonico@gmail.com>
2019-01-15 21:23:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+if array.size == 1
+  return array[0]
+elsif array.size == 2
+  return "#{array[0]} and #{array[1]}"
+end
+last_word = array.pop
+output1 = array.join(", ")
+output2 = output1+", and #{last_word}"
+return output2
 end
